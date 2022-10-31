38e40788f9ba412ab1bc934af07a34b47d10a3a5
hubert <kuchhubert@gmail.com>
2022-10-31 11:59:42 +0100

    Changed context menu color to theme color

--- src/components/contextMenu/ContextMenuElement.tsx
+++ src/components/contextMenu/ContextMenuElement.tsx
@@ -3,7 +3,11 @@ import ChosenThemeSettings from "../../settings/ChosenThemeSettings";
 
 const ContextMenuElement = forwardRef<HTMLDivElement>((props, ref) => {
     return (
-        <div ref={ref} className="context_menu hidden absolute left-1/2 top-1/2 w-64 h-80 rounded p-2" style={{background: ChosenThemeSettings.CONTEXT_MENU_COLOR}}>
+        <div
+            ref={ref}
+            className="context_menu hidden absolute left-1/2 top-1/2 w-64 h-80 rounded p-2"
+            style={{background: ChosenThemeSettings.CONTEXT_MENU_COLOR, color: ChosenThemeSettings.BASE_FONT_COLOR}}
+        >
             test
         </div>
     );
