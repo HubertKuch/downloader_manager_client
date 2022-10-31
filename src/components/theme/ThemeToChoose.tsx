31d2c5782512ccb950cf059564863354762be6cf
hubert <kuchhubert@gmail.com>
2022-10-31 10:44:21 +0100

    Added appearance settings; choosing theme

---
+++ src/components/theme/ThemeToChoose.tsx
@@ -0,0 +1,27 @@
+import React, {useLayoutEffect, useRef} from "react";
+import ThemeSettings from "../../settings/ThemeSettings";
+
+interface ThemeToChooseProps {
+    theme: ThemeSettings;
+}
+
+export default function ThemeToChoose(props: ThemeToChooseProps): JSX.Element {
+
+    const ref = useRef<HTMLDivElement>();
+
+    useLayoutEffect(() => {
+
+        ref.current.addEventListener("click", () => {
+            localStorage.setItem("theme", props.theme.NAME);
+            window.location.reload();
+        })
+
+    }, [ ref ]);
+
+    return (
+        <div ref={ref}
+             className={"rounded-full h-10 w-10 m-2 relative inline-block cursor-pointer"}
+             style={{background: props.theme.DOMAIN_COLOR}}
+        />
+    );
+}
--- src/views/Settings.tsx
+++ src/views/Settings.tsx
@@ -1,30 +1,34 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import MainLayout from "../components/layouts/MainLayout";
 import MainNav from "../components/nav/MainNav";
-import {DEFAULT_USER, User} from "../models/User";
+import {User} from "../models/User";
 import UserAPIConsumer from "../api/UserAPIConsumer";
+import HeaderText from "../components/utils/HeaderText";
+import ThemeToChoose from "../components/theme/ThemeToChoose";
+import ChosenThemeSettings from "../settings/ChosenThemeSettings";
+import AvailableThemeSettings from "../settings/AvailableThemeSettings";
 
 export default function Settings(): JSX.Element {
-    const [user, setUser] = useState<User>(DEFAULT_USER);
-    const getUser = () => {
-        const get = async () => {
-            return await UserAPIConsumer.getLoggedInUser();
-        }
-
-        useEffect(() => {
-            get().then(r => {
-                setUser(r)
-
-            });
-            return () => {};
-        }, []);
-    }
-
-    getUser()
+    const [user, setUser] = useState<User>(UserAPIConsumer.getUserFromStorage);
 
     return (
         <>
             <MainLayout nav={<MainNav user={user} />}>
+                <HeaderText text={"Settings"} />
+
+                <br/>
+
+                <main>
+                    <div>
+                        <h2 className={"text-2xl"}>Appearance</h2>
+                    </div>
+                    <div>
+                        <h2 className={""}>Choose your theme</h2>
+                        <div>
+                            { AvailableThemeSettings.map(theme => <ThemeToChoose theme={theme} />) }
+                        </div>
+                    </div>
+                </main>
 
             </MainLayout>
         </>
