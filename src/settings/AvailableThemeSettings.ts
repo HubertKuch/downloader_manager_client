5fd56a563f73d68c509d7805b4cec9b71fa08add
hubert <kuchhubert@gmail.com>
2022-11-07 16:00:57 +0100

    Modified light theme

--- src/settings/AvailableThemeSettings.ts
+++ src/settings/AvailableThemeSettings.ts
@@ -2,7 +2,7 @@ import ThemeSettings from "./ThemeSettings";
 
 export default [
     {
-        NAME: "LIGHT_GREEN",
+        NAME: "DARK_GREEN",
         BASE_FONT_COLOR: "#fff",
         DOMAIN_COLOR: "#22a97c",
         FILE_COLOR: "#22a97c",
@@ -32,18 +32,18 @@ export default [
         CONTEXT_MENU_COLOR: "#242336"
     },
     {
-        NAME: "LIGHT",
+        NAME: "LIGHT_GREEN",
         BASE_FONT_COLOR: "#000",
-        DOMAIN_COLOR: "#fff",
-        FILE_COLOR: "#286fba",
-        FOLDER_COLOR: "#286fba",
+        DOMAIN_COLOR: "#fdae37",
+        FILE_COLOR: "#66b3a1",
+        FOLDER_COLOR: "#66b3a1",
         PROGRESS_BAR_FRONT: "#47bfdb",
         PROGRESS_BAR_BACK: "#e2e9e2",
         BUTTON_COLOR: "#131c2d",
         SUBMIT_BUTTON_COLOR: "#131c2d",
         INPUT_BACKGROUND_COLOR: "#131c2d",
         CONTEXT_MENU_COLOR: "#131c2d",
-        SECONDARY_BACKGROUND_COLOR: "#333f55",
-        PRIMARY_BACKGROUND_COLOR: "#f9f9f9"
-    }
+        SECONDARY_BACKGROUND_COLOR: "#2c6873",
+        PRIMARY_BACKGROUND_COLOR: "#fff"
+    },
 ] as ThemeSettings[];
